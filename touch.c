b6d8c99da360eb647e69548facae37ac121bbd65
jflora96 <45500854+jflora96@users.noreply.github.com>
2021-03-30 20:11:05 -0700

    Add files via upload

---
+++ touch.c
@@ -0,0 +1,32 @@
+//touch.c
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+char buf[512];
+int
+main(int argc, char *argv[])
+{
+	int i;
+	if (argc == 1)
+	{
+		printf(1, "touch SOURCE FILE");
+		exit();
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		int file;
+		if ((file = open(argv[i], O_RDONLY)) < 0)
+		{
+			file = open(argv[i], O_CREATE|O_RDONLY);
+			printf(1, "created %d", argv[i]);
+		}
+		else
+		{
+			printf(1, "failed to create %d", argv[i]);
+		}
+		close(file);
+	}
+	exit();
+}
